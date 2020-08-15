a18a9cfab6e030bbbe80c1225e1a211f2f61dac0
aman-d-1-n-only <abhitheone456@gmail.com>
2020-08-15 23:49:28 +0530

    Installed Basic assets required

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const path = require('path');
 const logger = require('morgan');
 
 const app = express();
 app.use(logger('dev'));
+app.use(express.static(path.join(__dirname, 'public')));
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res, next) => {
